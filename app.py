5fb0a78e7b15d9427c558c71f71c30bcf443d7cd
Guilherme Da Gama Pimenta <guilherme44pimenta@gmail.com>
2025-10-12 03:31:57 -0300

    Início da home page e da implementação do BD

--- app.py
+++ app.py
@@ -1,7 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template
+#from typing import Optional
+#from dataclasses import dataclass, field
+from DB import getTaskList
+
 
 app = Flask(__name__)
 
+
+
+#lista_tarefas = [["Caminhar no parque", True],["Jogar futebol", False],["Ir ao mercado", True]]
+lista_de_tarefas = getTaskList()
+'''@dataclass(frozen=False)
+class Tarefas:
+    nome: str
+    concluido: bool
+    data_prazo: str
+        
+lista_tarefas = [
+    Tarefas("Caminhar no parque", False, "19/09/2006"),
+    Tarefas("Jogar videogame", True, "20/05/2026"),
+    Tarefas("Buscar cola", False, "11/09/2001")
+]
+
+t1 = lista_tarefas[0]
+t2 = lista_tarefas[1]
+t3 = lista_tarefas[2]'''
+
+
+
+
 @app.route('/')
-def hello_world():
-    return 'Olá do Flask rodando no Arch Linux (WSL 2)!'
+def home_pg():
+   #return render_template("lista_tarefas.html", tarefas=lista_tarefas)
+    return render_template("lista_tarefas.html", tarefas=lista_de_tarefas)
+
+app.run()

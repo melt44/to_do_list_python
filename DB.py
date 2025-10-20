a51e3ef179d337f19eaf7815c86c29e4290bb416
Guilherme Da Gama Pimenta <guilherme44pimenta@gmail.com>
2025-10-20 11:31:19 -0300

    Reescrita para melhor legibilidade e performance

--- DB.py
+++ DB.py
@@ -15,15 +15,25 @@ db_pw = os.getenv("db_pw")
 def getTaskList():
     conn = psycopg2.connect(dbname=db_name, user=db_user, host=db_host, password=db_pw)
     cur = conn.cursor()
-    cur.execute('SELECT nome_task, is_done FROM public."TaskList";')
+    cur.execute('SELECT ID, nome_task, is_done FROM public."TaskList";')
     lista_de_tarefas = cur.fetchall()
-    cur.close
-    conn.close
+    cur.close()
+    conn.close()
     return lista_de_tarefas
 
 def addTask(nome, data):
+    executeQuery('INSERT INTO public."TaskList"(nome_task, data_fim) values(\'%s\', \'%s\');commit;' % (nome, data))
+
+def updateTask(nome, ID):
+    executeQuery('UPDATE public."TaskList" SET nome_task=\'%s\' WHERE ID=%s;' % (nome, ID))
+
+def deleteTask(ID):
+    executeQuery('DELETE FROM public."TaskList" WHERE ID=%s;' % (ID))
+
+def executeQuery(query):
     conn = psycopg2.connect(dbname=db_name, user=db_user, host=db_host, password=db_pw)
     cur = conn.cursor()
-    cur.execute('INSERT INTO public."TaskList"(nome_task, data_fim) values(\'%s\', \'%s\');commit;' % (nome, data))
-    cur.close
-    conn.close
+    cur.execute(query)
+    conn.commit()
+    cur.close()
+    conn.close()
